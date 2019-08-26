a039b301d43246711d8549612af4aa18c94a1d27
Adam <adamblanchet@free.fr>
2019-08-26 21:57:33 +0200

    first playable version 2

--- README.md
+++ README.md
@@ -2,19 +2,14 @@
 > Simple online pong game made with pygame  
 > Server hosts the game, two clients play, connecting to the server
 
+## About this project
+This project is by no means complete, polished or efficient.
+However it accomplishes to create a simple pong experience. Feel free to try it out!
 
 ## Usage
-### UDP Version
-> Server : python server_udp.py [host] [port]
-> Clients : pytho server_udp.py [host] [port]  
-> Host and port should match the ones used to start server  
-> Using UDP should be preferred, as it should be less laggy across laggy connections  
-### TCP Version (old)
-> You can always use the old tcp version of the game :  
-> Server : python server.py [host] [port]  
-> Clients : python client.py [host] [port]  
 
-## Requirements
-> Python 3 or above (tested on Python 3.7.1)  
-> Pygame module  
-> For use over the internet, the machine the server runs on will have to have port-forwarding enabled
+### Server
+`python3 server.py <host> <port>`
+
+### Client
+`python3 client.py <host> <port>`
--- server.py
+++ server.py
@@ -1,163 +1,53 @@
-import socket
-import threading
-import time
-import pygame
 import sys
-from random import choice
+import socket
+import time
+from sessions import *
+
+if len(sys.argv) != 3:
+    print("Correct usage : python3 server.py <host> <port>")
+    sys.exit()
+
+HOST, PORT = sys.argv[1], int(sys.argv[2])
+
+listen_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+listen_socket.bind((HOST, PORT))
+listen_socket.settimeout(2)
 
 
-class Ball(object):
+"""
+Sessions
+"""
 
-    def __init__(self):
-        self.rect = pygame.rect.Rect(400-7, 400-7, 15, 15)
-        self.x = 400
-        self.y = 400
-        self.velx = choice((3, -3))
-        self.vely = choice((4, -4))
-        self.size = 15
+sessions_list = list()
+session_threads = list()
+current_session_index = 0 # The index of the current session
+sessions_list.append(Session(current_session_index))
+while True:
 
-    def reset(self):
-        self.__init__()
-        time.sleep(2)
+    try:
+        data, addr = listen_socket.recvfrom(2048)
 
-    def move(self):
-        #self.x += self.velx
-        #self.y += self.vely
-        self.rect.move_ip(self.velx, self.vely)
-        if self.rect.y > 800 - self.size:
-            self.vely = -(abs(self.vely))
-        elif self.rect.y < 0:
-            self.vely = abs(self.vely)
-        if self.rect.x > 800 - self.size:
-            self.reset()
-        elif self.rect.x < 0:
-            self.reset()
-        #   print(self.x,self.y)
-        if abs(self.velx) < 40:
-            self.velx *= 1.001
-        global player1
-        global player2
+        data = data.decode()
 
-        if self.rect.colliderect(player1.rect):
-            self.velx = abs(self.velx)
-            self.vely = (self.rect.centery - player1.rect.centery) * 0.1 
-        elif self.rect.colliderect(player2.rect):
-            self.velx = -abs(self.velx)
-            self.vely = (self.rect.centery - player2.rect.centery) * 0.1
+        if data == "new":
 
+            print("Adding {}:{} to session {}\n".format(addr[0], addr[1], current_session_index))
+            sessions_list[current_session_index].add_member(Member(addr))
+            if len(sessions_list[current_session_index]) >= 2: # Creates new session when current one is full
+                print("Creating a new session and starting current one")
+                sessions_list[current_session_index].start()
+                current_session_index += 1
+                sessions_list.append(Session(current_session_index))
 
-class Player(object):
-
-    def __init__(self, player):
-
-        self.player = player
-        
-        if self.player == 1:
-            x = 10
-        elif self.player == 2:
-            x = 800 - 10
         else:
-            x = 400
-            print("Player isn't 1 or 2")
-        
-        y = 400 - 30
-
-        self.rect = pygame.rect.Rect(x, y, 20, 200)
-
-class server_thread(threading.Thread):
-
-    def __init__(self, sock, addr, p):
-        threading.Thread.__init__(self)
-        self.sock = sock
-        self.host, self.port = addr
-        self.p = p
-        print("Connection on ", host, port)
-
-    def run(self):
-        global ball
-        global running
-        global player1
-        global player2
-        while running:
-            try:
-    
-                data = str(ball.rect.x + 100) + "x" + str(ball.rect.y + 100)
-                if self.p == 1:
-                    data += "x" + str(player2.rect.x) + "x" + str(player2.rect.y)
-                elif self.p == 2:
-                    data += "x" + str(player1.rect.x) + "x" + str(player1.rect.y)
-                self.sock.sendall(data.encode())
-            except:
-                print("The program has encountered an error : sending data")
-                running = False
-
-            try:
-                data = self.sock.recv(2048).decode()
-                coords = data.split("x")
-                if self.p == 1:
-                    player1.rect.x = int(coords[0])
-                    player1.rect.y = int(coords[1])
-                else:
-                    player2.rect.x = int(coords[0])
-                    player2.rect.y = int(coords[1])
-                    
-
-            except:
-                print("The program has encountered an error : receiving data")
-
-        self.sock.close()
-
-class game_updater(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        global running
-        while running:
-            time.sleep(1/60)
-            global ball
-            ball.move() 
+            msg = data.split(":")
+            if len(msg) == 4:
+                try:
+                    sessions_list[int(msg[0])].update_player(msg)
+                except Exception as e:
+                    print(str(e) + "player {}".format(msg[1]))
+                    time.sleep(1)
 
 
-### Game Data ###
-player1 = Player(1)
-player2 = Player(2)
-ball = Ball()
-running = True
-
-
-
-###########################
-
-# Initialize server
-host = sys.argv[1]
-port = int(sys.argv[2])
-
-
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-lsock.bind((host, port))
-
-
-lsock.listen(1)
-
-ssock, addr = lsock.accept()
-ssock.send("1".encode())
-print("Player 1 connected")
-sthread = server_thread(ssock, addr, 1)
-
-lsock.listen(1)
-
-ssock2, addr2 = lsock.accept()
-ssock2.send("2".encode())
-print("Player 2 connected")
-sthread2 = server_thread(ssock2, addr2, 2) 
-gthread = game_updater()
-
-gthread.start()
-sthread.start()
-sthread2.start()
-sthread.join()
-sthread2.join()
-gthread.join()
+    except socket.timeout:
+        print("No incoming connection \r", end="")
